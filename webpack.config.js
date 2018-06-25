97724a2158ed01e1789643397668846c4a6df811
wangwq <80944188@qq.com>
2018-06-25 13:45:59 +0800

    html-webpack-plugin的简单实用

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,12 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js' 
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: path.resolve(__dirname, './src/index.html'),
+            title: 'webpack demo'
+        })
+    ]
 }
